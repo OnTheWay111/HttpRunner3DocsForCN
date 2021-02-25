5b1d58a8284e3f73ee0740c4919de4ac11e06a62
lizhen1 <lizhen1@cmcm.com>
2021-02-25 18:14:11 +0800

    first commit

---
+++ js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?64ae7cd356095ffe37f635a2b38d97c9";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
